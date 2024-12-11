de83a7f6263ca99aad9f4ed97a221cf1db212e28
Kaushik Chowdhury <kaushikchowdhury231@gmail.com>
2024-12-12 05:13:23 +0530

    renamed nvim-dap

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -5,9 +5,14 @@ return {
 	dependencies = {
 		'nvim-lua/plenary.nvim',
 		'stevearc/dressing.nvim', -- optional for vim.ui.select
+		'mfussenegger/nvim-dap',
 	},
 	config = function()
-		require("flutter-tools").setup {}
+		require("flutter-tools").setup {
+			debugger = {
+				enabled = true,
+			}
+		}
 		vim.keymap.set('n', '<leader>fn', require('telescope').extensions.flutter.commands, { desc = "Load flutter tools" })
 		vim.keymap.set('n', '<leader>fm', require('telescope').extensions.flutter.fvm, { desc = "Load fvm tools" })
 	end
