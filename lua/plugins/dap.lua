9583fc175d958a21a0ee22bde4f5678b909ee25c
Kaushik Chowdhury <kaushikchowdhury231@gmail.com>
2024-12-12 05:09:02 +0530

    Added dap support

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -12,9 +12,22 @@ return {
 			"nvim-neotest/nvim-nio"
 		},
 		config = function()
-			local dapui = require('dapui')
+			local dap, dapui = require("dap"), require("dapui")
 			dapui.setup()
 			vim.keymap.set('n', '<leader>du', dapui.toggle, { desc = "Toggle DAP UI" })
+
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
 		end
 	},
 	{
