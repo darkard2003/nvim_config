e060c1e378ebb0cc99a11ab715832ee77c938d7e
Kaushik Chowdhury <kaushikchowdhury231@gmail.com>
2024-12-15 01:18:00 +0530

    Some changes in dap

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,37 +1,39 @@
 return {
-	{
-		'mfussenegger/nvim-dap',
-		config = function()
-			require 'config.nvim-dap'
-		end
-	},
-	{
-		"rcarriga/nvim-dap-ui",
-		dependencies = {
-			"mfussenegger/nvim-dap",
-			"nvim-neotest/nvim-nio"
-		},
-		config = function()
-			local dap, dapui = require("dap"), require("dapui")
-			dapui.setup()
-			vim.keymap.set('n', '<leader>du', dapui.toggle, { desc = "Toggle DAP UI" })
+  {
+    'mfussenegger/nvim-dap',
+    config = function()
+      require 'config.nvim-dap'
+    end
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio"
+    },
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
+      dapui.setup()
+      vim.keymap.set('n', '<leader>du', dapui.toggle, { desc = "Toggle DAP UI" })
 
-			dap.listeners.before.attach.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				dapui.close()
-			end
-		end
-	},
-	{
-		'leoluz/nvim-dap-go',
-		opts = {},
-	}
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    end
+  },
+  {
+    'leoluz/nvim-dap-go',
+    lazy = true,
+    ft = 'go',
+    opts = {},
+  }
 }
